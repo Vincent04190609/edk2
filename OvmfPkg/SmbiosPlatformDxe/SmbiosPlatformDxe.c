5da3169f7ee157153b08eff87f0d6350475ba279
Fleet BIOS Agent <fleet@local>
2026-06-10 22:15:40 +0800

    JIRA-539: Set SMBIOS Type 1 product name to Onboarding and release T92.
    Update SmbiosPlatformDxe Type 1 ProductStr and bump test BIOS version PCDs so inventory tools see the Onboarding system product name.

--- OvmfPkg/SmbiosPlatformDxe/SmbiosPlatformDxe.c
+++ OvmfPkg/SmbiosPlatformDxe/SmbiosPlatformDxe.c
@@ -212,7 +212,7 @@ InstallAllStructures (
   {
     CHAR8   *Type1;
     CHAR8   *ManuStr      = "Vibe-Factory";
-    CHAR8   *ProductStr   = "OVMF";
+    CHAR8   *ProductStr   = "Onboarding";
     CHAR8   *VersionStr   = "1.0";
     CHAR8   *SerialStr    = "0FEDCBA987654321";
     UINTN   ManuLen       = AsciiStrLen (ManuStr);
